33aeba84bdc8e367705669c61f743f76ec5e3470
unquire <82368637+unquire@users.noreply.github.com>
2024-05-11 16:39:49 +0800

    Add files via upload
    启动模块

---
+++ daocao_starter/src/main/java/com/daocao/DaoCaoApplication.java
@@ -0,0 +1,18 @@
+package com.daocao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author:啦啦啦
+ * Package:com.xtt.starter
+ * Project:daocao_study
+ * name:DaoCaoApplication
+ * Date:2024/3/10 20:52
+ */
+@SpringBootApplication
+public class DaoCaoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DaoCaoApplication.class,args);
+	}
+}
